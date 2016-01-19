e59c8e958d94167070632bec9b098311cb4c9a3d
Ben on windows <ben@ajaxscape.com>
2016-01-19 07:55:36 +0000

    Clean-up cucumber tests

--- features/step_definitions/form_steps.js
+++ features/step_definitions/form_steps.js
@@ -16,70 +16,38 @@ module.exports = function () {
 
     this.setDefaultTimeout(60000);
 
-    this.When(/^I am using a desktop$/,
-        function (next) {
-            this.setSize(1024, 768);
-            next()
-        }
-    );
+    this.When(/^I am using a desktop$/, function () {
+        return this.setSize(1024, 768);
+    });
 
-    this.When(/^I visit the (.*) page$/,
-        function (page, next) {
-            this.visit(page)
-                .then(next)
-                .catch(next)
-        }
-    );
+    this.When(/^I visit the (.*) page$/, function (page) {
+        return this.visit(page);
+    });
 
-    this.Then(/^I should eventually be on the (.*) page$/,
-        function (page, next) {
-            this.whenPageIs(page)
-                .then(next)
-                .catch(next)
-        }
-    );
+    this.Then(/^I should eventually be on the (.*) page$/, function (page) {
+        return this.whenPageIs(page);
+    });
 
-    this.When(/^I click the (.*)$/,
-        function (id, next) {
-            this.click(id)
-                .then(next)
-        }
-    );
+    this.When(/^I click the (.*)$/, function (id) {
+        return this.click(id);
+    });
 
-    this.When(/^I type "(.*)" into the (.*)$/,
-        function (text, id, next) {
-            this.sendKeys(id, text)
-                .then(next)
-        }
-    );
+    this.When(/^I type "(.*)" into the (.*)$/, function (text, id) {
+        return this.sendKeys(id, text)
+    });
 
-    this.Then(/^the value of the (.*) should be "(.*)"$/,
-        function (id, expectedVal, next) {
-            this.getVal(id)
-                .then(val => {
-                    expect(val).to.equal(expectedVal);
-                    next()
-                })
-                .catch(next)
-        }
-    );
+    this.Then(/^the value of the (.*) should be "(.*)"$/, function (id, expectedVal) {
+        return this.getVal(id)
+            .then(val => expect(val).to.equal(expectedVal));
+    });
 
-    this.Then(/^the (.*) text should contain "(.*)"$/,
-        function (id, expectedText, next) {
-            this.getText(id)
-                .then(text => {
-                    expect(text).to.contain(expectedText);
-                    next()
-                })
-                .catch(next)
-        }
-    );
+    this.Then(/^the (.*) text should contain "(.*)"$/, function (id, expectedText) {
+        return this.getText(id)
+            .then(text => expect(text).to.contain(expectedText));
+    });
 
-    this.Then(/^the (.*) should eventually be "([^"]*)"$/,
-        function (id, text, next) {
-            this.whenTextMatches(id, text)
-                .then(next);
-        }
-    );
+    this.Then(/^the (.*) should eventually be "([^"]*)"$/, function (id, text) {
+        return this.whenTextMatches(id, text);
+    });
 
 };
