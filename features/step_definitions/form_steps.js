e8d20986787c2333f5567f03867dc40e2b91f97e
Ben on windows <ben@ajaxscape.com>
2016-02-24 07:18:08 +0000

    Clean up select

--- features/step_definitions/form_steps.js
+++ features/step_definitions/form_steps.js
@@ -2,7 +2,6 @@
 
 var chai = require('chai');
 var chaiAsPromised = require('chai-as-promised');
-var cheerio = require('cheerio');
 var cucumber_partner = require('../../lib/world');
 
 chai.use(chaiAsPromised);
@@ -12,51 +11,47 @@ cucumber_partner.setConfig({host: 'http://localhost:3021'});
 
 module.exports = function () {
 
-    this.World = cucumber_partner.World; // overwrite default World constructor
+  this.World = cucumber_partner.World; // overwrite default World constructor
 
-    this.setDefaultTimeout(60000);
+  this.setDefaultTimeout(60000);
 
-    this.When(/^I am using a desktop$/, function () {
-        return this.setSize(1024, 768);
-    });
+  this.When(/^I am using a desktop$/, function () {
+    return this.setSize(1024, 768);
+  });
 
-    this.When(/^I visit the (.*) page$/, function (page) {
-        return this.visit(page);
-    });
+  this.When(/^I visit the (.*) page$/, function (page) {
+    return this.visit(page);
+  });
 
-    this.Then(/^I should eventually be on the (.*) page$/, function (page) {
-        return this.whenPageIs(page);
-    });
+  this.Then(/^I should eventually be on the (.*) page$/, function (page) {
+    return this.whenPageIs(page);
+  });
 
-    this.When(/^I click the (.*)$/, function (id) {
-        return this.click(id);
-    });
+  this.When(/^I click the (.*)$/, function (id) {
+    return this.click(id);
+  });
 
-    this.When(/^I type "(.*)" into the (.*)$/, function (text, id) {
-        return this.sendKeys(id, text);
-    });
+  this.When(/^I type "(.*)" into the (.*)$/, function (text, id) {
+    return this.sendKeys(id, text);
+  });
 
-    this.Then(/^the value of the (.*) should be "(.*)"$/, function (id, expectedVal) {
-        return this.getVal(id).should.eventually.equal(expectedVal);
-    });
+  this.Then(/^the value of the (.*) should be "(.*)"$/, function (id, expectedVal) {
+    return this.getVal(id).should.eventually.equal(expectedVal);
+  });
 
-    this.Then(/^the (.*) text should contain "(.*)"$/, function (id, expectedText) {
-        return this.getText(id).should.eventually.contain(expectedText);
-    });
+  this.Then(/^the (.*) text should contain "(.*)"$/, function (id, expectedText) {
+    return this.getText(id).should.eventually.contain(expectedText);
+  });
 
-    this.Then(/^the (.*) should eventually be "([^"]*)"$/, function (id, text) {
-        return this.whenTextMatches(id, text);
-    });
+  this.Then(/^the (.*) should eventually be "([^"]*)"$/, function (id, text) {
+    return this.whenTextMatches(id, text);
+  });
 
-    this.Then(/^the (.*) should not exist$/, function (id) {
-        return this.notFind(id);
-    });
+  this.Then(/^the (.*) should not exist$/, function (id) {
+    return this.notFind(id);
+  });
 
-    this.Then(/^I expect to see a list of (\d+) (.*)$/, function (count, id) {
-        return this.getHtml(id)
-          .then(html => {
-              var $ = cheerio.load(html);
-              ($('li').length).should.equal(parseInt(count))
-          })
-    });
+  this.Then(/^I expect to see a list of (\d+) (.*)$/, function (count, id) {
+    return this.select(id, 'li').should.eventually.have.length(parseInt(count))
+  });
 };
