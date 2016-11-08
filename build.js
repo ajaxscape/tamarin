7d84e284d0664e2b87e2ff7a0ccef0c15cb0d468
ajaxscape <ben@ajaxscape.com>
2016-11-08 08:41:39 +0000

    auto gen

--- build.js
+++ build.js
@@ -21,7 +21,7 @@ const structure = {
 function generate (dir, node) {
   const keys = Object.keys(node)
   console.log(keys)
-  keys.each((id) => {
+  keys.forEach((id) => {
     console.log(`Processing: ${id}`)
     if (_.isString(node[id])) {
       const filename = `${dir}/${id}.js`
