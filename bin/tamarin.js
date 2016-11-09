20397d384333592951302fb33a52077e76b2b740
Ben on windows <ben@ajaxscape.com>
2016-11-09 07:19:21 +0000

    Generating a tamarin test

--- bin/tamarin.js
+++ bin/tamarin.js
@@ -1,6 +1,27 @@
 #!/usr/bin/env node
 
+/**
+ * Module dependencies.
+ */
+
+const fs = require('fs')
+const program = require('commander')
 const build = require('./build')
 
-console.log('building!')
-build()
+const packageFile = fs.readFileSync(`${process.cwd()}/package.json`, 'utf8')
+const json = JSON.parse(packageFile)
+const version = json.version
+
+program
+  .version(version)
+  .option('-b, --build', 'Build example features')
+  .option('-t, --test', 'Start test')
+  .parse(process.argv)
+
+if (program.build) {
+  build()
+}
+
+if (program.test) {
+  console.log('test')
+}
